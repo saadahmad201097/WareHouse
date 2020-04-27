b39db49c553909c4def6a67fa28c1e0a766cb875
saqibJamil <saqib.jamil@appifytech.com>
2020-04-27 11:55:28 +0500

    Implement delete api

--- src/public/endpoins.js
+++ src/public/endpoins.js
@@ -1,10 +1,10 @@
 const baseUrl= 'http://localhost:8080/api';
 
 // auth endpoints
-export const loginUrl = baseUrl+'/auth/login';
+export const loginUrl = `${baseUrl}/auth/login`;
 
 // items endpoints
-export const getItemsUrl = baseUrl+'/item/getitems';
-export const addItemUrl = baseUrl+'/item/additem';
-export const updateItemUrl = baseUrl+'/item/updateitem';
-export const deleteItemUrl = baseUrl+'/item/deleteitem';
+export const getItemsUrl = `${baseUrl}/item/getitems`;
+export const addItemUrl = `${baseUrl}/item/additem`;
+export const updateItemUrl = `${baseUrl}/item/updateitem`;
+export const deleteItemUrl = `${baseUrl}/item/deleteitem`;
