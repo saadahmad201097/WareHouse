c4644199dbc7ea608f98d32edeea0278c56d2d2a
saqibJamil <saqib.jamil@appifytech.com>
2020-05-18 13:22:30 +0500

    Work on PWA

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Redirect } from 'react-router-dom';
-
 // core components
 import Admin from 'layouts/Admin.js';
 import 'assets/css/material-dashboard-react.css?v=1.8.0';
@@ -29,6 +28,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import axios from 'axios';
 import cookie from 'react-cookies';
 import { ToastsContainer, ToastsStore } from 'react-toasts';
+import * as serviceWorker from './serviceWorker';
 import Login from './layouts/Login';
 import configureStore, { history } from './store';
 
@@ -124,3 +124,7 @@ const MainApp = () => {
 };
 
 ReactDOM.render(<MainApp />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
